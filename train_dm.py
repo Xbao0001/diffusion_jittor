306826094e6120c3a4d935092834015afb54ed45
wangzhj <zhoujiewang@njust.edu.cn>
2022-08-02 14:14:58 +0000

    update

--- train_dm.py
+++ train_dm.py
@@ -39,6 +39,7 @@ def init_and_run(cfg):
 
 
 def main(cfg):
+    # TODO: add ema, before ema, remeber to sync
     # dataloader
     train_loader, _ = get_cifar10_dataloader(**cfg.data)
 
@@ -69,6 +70,7 @@ def main(cfg):
             loss = model(img)
             optimizer.step(loss)
 
+            jt.sync_all()
             if global_train_steps % cfg.print_freq == 0:
                 print(
                     f"epoch: {epoch:3d}\t", 
@@ -86,21 +88,21 @@ def main(cfg):
         
         # sample val set
         if epoch % cfg.sample_freq == 0: 
-            model.eval()
-            img_sample = model.ddim_sample(shape=(64, 3, 32, 32), sampling_steps=100)
-
             if jt.rank == 0 and cfg.wandb:
+                model.eval()
+                img_sample = model.p_sample_loop(shape=(36, 3, 32, 32))
                 wandb.log({
-                    'generated': wandb.Image(make_grid(img_sample.data, n_cols=8)),
-                    'original': wandb.Image(make_grid(img.data[:64], n_cols=8)),
+                    'generated': wandb.Image(make_grid(img_sample.data, n_cols=6)),
+                    'original': wandb.Image(make_grid(img.data[:36], n_cols=6)),
                 })
 
+        jt.sync_all()
         if epoch % cfg.save_freq == 0:
             save_checkpoint({
                 'epoch': epoch,
                 'model': model.state_dict(),
                 'optimizer': optimizer.state_dict(),
-            }, save_dir=cfg.save_dir, filename=f"epoch_{epoch}.ckpt")
+            }, save_dir=cfg.save_dir, filename=f"last.ckpt")
 
 
 if __name__ == "__main__":
